e05451e91118c884d14143cdfa6434824d8516af
Tarek <itcameranian@gmail.com>
2022-08-22 12:00:38 +0900

    #6 Hooks、取得

---
+++ client/src/hooks/useTweet.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react"
+import { ulid } from "ulid"
+
+import * as tweetData from "../apis/Tweets"
+import { Tweet } from "../types/Tweet"
+
+export const useTweet = () => {
+    const [tweetList, setTweetList] = useState<Tweet[]>([])
+
+    useEffect(() => {
+        tweetData.getAllTweets().then((tweet) => {
+            console.log(...tweet)
+            setTweetList([...tweet].reverse)
+        })
+    },[])
+
+    return {tweetList}
+}
