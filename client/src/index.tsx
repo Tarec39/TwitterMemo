0bc413f14bc8d3071c304bffce929174e5d5dc9f
Tarek <itcameranian@gmail.com>
2022-08-24 09:26:05 +0900

    #6 [SUCCESS] 出力できた。

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,16 @@
 import { createRoot } from 'react-dom/client'
+
 import { TweetList } from './components/Output/TweetList'
 import { useTweet } from './hooks/useTweet'
 
+import { TestType } from './components/Output/TestType'
+
 const App = () => {
-  const {tweetList} = useTweet()
+  const {test, tweetList} = useTweet()
   return(
     <>
-      {/* <TweetList tweetList={tweetList}/> */}
+      <TweetList tweetList={tweetList}/>
+      <TestType onClick={test}/>
     </>
   )
 }
