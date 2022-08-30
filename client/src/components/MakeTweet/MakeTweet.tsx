9fe6e13b3a68d6ee457c189c41a180725423451f
Tarek <itcameranian@gmail.com>
2022-08-30 11:22:48 +0900

    [update] 加筆

--- client/src/components/MakeTweet/MakeTweet.tsx
+++ client/src/components/MakeTweet/MakeTweet.tsx
@@ -9,7 +9,7 @@ export const MakeTweet = (props: Props) => {
     return(
         <>
         <h2>ツイートの作成</h2>
-        <textarea ref={props.inputEl}></textarea>
+        <textarea ref={props.inputEl} style={{resize:'none', width:'300px', height:'200px'}}></textarea>
         <button onClick={props.onClick}>ツイート</button>
         </>
     )
