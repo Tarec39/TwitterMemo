52d9c9353e3b25c52b773f91a591079344d059bf
Tarek <itcameranian@gmail.com>
2022-08-31 08:52:27 +0900

    #8 文字数を表示する機能ができた。

--- client/src/components/MakeTweet/MakeTweet.tsx
+++ client/src/components/MakeTweet/MakeTweet.tsx
@@ -3,13 +3,14 @@ import { RefObject } from "react"
 type Props = {
     inputEl: RefObject<HTMLTextAreaElement>
     onClick: () => any
+    onChange: () => any
 }
 
 export const MakeTweet = (props: Props) => {
     return(
         <>
         <h2>ツイートの作成</h2>
-        <textarea ref={props.inputEl} style={{resize:'none', width:'300px', height:'200px'}}></textarea>
+        <textarea onChange={props.onChange} ref={props.inputEl} style={{resize:'none', width:'300px', height:'200px'}}></textarea>
         <button onClick={props.onClick}>ツイート</button>
         </>
     )
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 import { useTweet } from './hooks/useTweet'
 
 import { TweetList } from './components/Output/TweetList'
 import { MakeTweet } from './components/MakeTweet/MakeTweet'
-
+import { WordCount } from './components/Indicator/WordCount'
 
 const App = () => {
   const {tweetList, addTweet} = useTweet()
@@ -17,10 +17,18 @@ const App = () => {
     addTweet(inputEl.current!.value)
     inputEl.current!.value = ""
   }
-  
+
+  const [WordNum, setWordNum] = useState(Number)
+
+  const countWords = () => {
+    if (inputEl.current?.value === "") setWordNum(0);
+    setWordNum(inputEl.current!.value.split('').length)
+  }
+
   return(
     <>
-      <MakeTweet inputEl={inputEl} onClick={handleAddTweet}></MakeTweet>
+      <MakeTweet inputEl={inputEl} onClick={handleAddTweet} onChange={countWords}></MakeTweet>
+      <WordCount WordNum={WordNum}></WordCount>
       <TweetList tweetList={tweetList}/>
     </>
   )
