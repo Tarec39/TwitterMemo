860e099fae02a742eafb872035b5b5f620b05091
Tarek Ito <itcameranian@gmail.com>
2022-09-02 21:20:31 +0900

    機能を示すコメントをつけた。

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,22 +9,27 @@ import { WordCount } from './components/Indicator/WordCount'
 
 const App = () => {
   const {tweetList, addTweet} = useTweet()
-
+  /**
+   * ツイート画面関連の機能
+   */
   const inputEl = useRef<HTMLTextAreaElement>(null)
 
+  /**ツイートを追加 */
   const handleAddTweet = () => {
     if(inputEl.current?.value === ""){return}
     addTweet(inputEl.current!.value)
     inputEl.current!.value = ""
   }
-
+  /**文字数を表示？ */
   const [WordNum, setWordNum] = useState(Number)
 
   const countWords = () => {
     if (inputEl.current?.value === "") setWordNum(0);
     setWordNum(inputEl.current!.value.split('').length)
   }
-
+  /**文字数を制限 */
+  //140文字超えたらボタンが押せなくなる。
+  
   return(
     <>
       <MakeTweet inputEl={inputEl} onClick={handleAddTweet} onChange={countWords}></MakeTweet>
