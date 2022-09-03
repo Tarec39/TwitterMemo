b5fe55b3b1e8e50a84f96bc7fa41fda04b7c2fe4
Tarek <itcameranian@gmail.com>
2022-09-03 12:50:30 +0900

    aaa

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,20 @@
 import { createRoot } from 'react-dom/client'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { useTweet } from './hooks/useTweet'
 
 import { TweetList } from './components/Output/TweetList'
 import { MakeTweet } from './components/MakeTweet/MakeTweet'
 import { WordCount } from './components/Indicator/WordCount'
+import { AddTweet } from './components/thread/AddTweet'
 
 const App = () => {
   const {tweetList, addTweet} = useTweet()
   /**
    * ツイート画面関連の機能
    */
+
+  //入力された文字
   const inputEl = useRef<HTMLTextAreaElement>(null)
 
   /**ツイートを追加 */
@@ -29,12 +32,28 @@ const App = () => {
   }
   /**文字数を制限 */
   //140文字超えたらボタンが押せなくなる。
-  
+  const [isDisabled, setIsDisabled] = useState<boolean>(false)
+
+  const handleisDisabled = () => {
+    if(WordNum > 140) return setIsDisabled(true);
+    else{return setIsDisabled(false)}
+  }
+
+  useEffect(()=>{
+    handleisDisabled()
+  },[WordNum])
+
+  /**スレッド機能 */
+  const addThread = () =>{
+    console.log("スレッドを追加します。")
+  }
+
   return(
     <>
-      <MakeTweet inputEl={inputEl} onClick={handleAddTweet} onChange={countWords}></MakeTweet>
+      <MakeTweet inputEl={inputEl} onClick={handleAddTweet} onChange={countWords} isDisabled={isDisabled}></MakeTweet>
       <WordCount WordNum={WordNum}></WordCount>
-      <TweetList tweetList={tweetList}/>
+      <AddTweet onClick={addThread}></AddTweet>
+      <TweetList tweetList={tweetList} onClick={addThread}/>
     </>
   )
 }
