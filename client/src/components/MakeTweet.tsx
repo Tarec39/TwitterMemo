1d952ab5dcb5ad1b110e4610fe3f47132c24ef42
Tarek <itcameranian@gmail.com>
2022-09-10 12:33:09 +0900

    必要な部品だけ残した。

--- client/src/components/MakeTweet.tsx
+++ client/src/components/MakeTweet.tsx
@@ -3,16 +3,18 @@ import { RefObject } from "react"
 type Props = {
     inputEl: RefObject<HTMLTextAreaElement>
     onClick: () => any
-    onChange: () => any
-    isDisabled: boolean
+    // onChange: () => any
+    // isDisabled: boolean
 }
 
 export const MakeTweet = (props: Props) => {
     return(
         <>
         <h2>ツイートの作成</h2>
-        <textarea onChange={props.onChange} ref={props.inputEl} style={{resize:'none', width:'300px', height:'200px'}}></textarea>
-        <button onClick={props.onClick} disabled={props.isDisabled}>ツイート</button>
+        {/* onChange={props.onChange} */}
+        <textarea  ref={props.inputEl} style={{resize:'none', width:'300px', height:'200px'}}></textarea>
+        {/* disabled={props.isDisabled} */}
+        <button onClick={props.onClick}>ツイート</button>
         </>
     )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -15,50 +15,24 @@ const App = () => {
    * ツイート画面関連の機能
    */
 
-  //入力された文字
-  const inputEl = useRef<HTMLTextAreaElement>(null)
+   const inputEl = useRef<HTMLTextAreaElement>(null)
 
-  /**ツイートを追加 */
-  const handleAddTweet = () => {
+   const handleAddTweet = () => {
     if(inputEl.current?.value === ""){return}
     addTweet(inputEl.current!.value)
     inputEl.current!.value = ""
   }
-  /**文字数を表示？ */
-  const [WordNum, setWordNum] = useState(Number)
-
-  const countWords = () => {
-    if (inputEl.current?.value === "") setWordNum(0);
-    setWordNum(inputEl.current!.value.split('').length)
-  }
-  /**文字数を制限 */
-  //140文字超えたらボタンが押せなくなる。
-  const [isDisabled, setIsDisabled] = useState<boolean>(false)
-
-  const handleisDisabled = () => {
-    if(WordNum > 140) return setIsDisabled(true);
-    else{return setIsDisabled(false)}
-  }
-
-  useEffect(()=>{
-    handleisDisabled()
-  },[WordNum])
 
   /**スレッド機能 */
   const addThread = () =>{
     console.log("追加します。")
-    return test
-  }
-
-  const test = () => {
-  return <MakeTweet inputEl={inputEl} onClick={handleAddTweet} onChange={countWords} isDisabled={isDisabled}></MakeTweet>
   }
 
   return(
     <>
-      <MakeTweet inputEl={inputEl} onClick={handleAddTweet} onChange={countWords} isDisabled={isDisabled}></MakeTweet>
-      <WordCount WordNum={WordNum}></WordCount>
-      <AddTweet onClick={addThread}></AddTweet>
+      <MakeTweet inputEl={inputEl} onClick={handleAddTweet}></MakeTweet>
+      {/* <WordCount WordNum={WordNum}></WordCount> */}
+      {/* <AddTweet onClick={addThread}></AddTweet> */}
       <TweetList tweetList={tweetList} deleteTweet={deleteTweet} onClick={addThread}/>
     </>
   )
