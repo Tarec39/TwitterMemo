9c99331dbf89adf98fb486c4668458447b9b6d5f
Tarek Ito <itcameranian@gmail.com>
2022-09-12 16:49:25 +0900

    useAddTweetBoxに加筆&整理

--- client/src/hooks/useAddTweetBox.tsx
+++ client/src/hooks/useAddTweetBox.tsx
@@ -2,12 +2,37 @@ import {useEffect,useState} from 'react'
 
 
 export const useAddTweetBox = () => {
+  /**
+   * useStateの定義
+   */
+  const [textAreaEl, setTextAreaEl] = useState<string>()
+  const [WordNum, setWordNum] = useState(Number)
+  const [isDisabled, setIsDisabled] = useState<boolean>(false)
 
-    const [textAreaEl, setTextAreaEl] = useState<string>()
+  /**
+   * 処理関数の定義
+   */
 
-    const onChangeTextAreaEl = (event:React.ChangeEvent<HTMLTextAreaElement>) => {
-      setTextAreaEl(event.target.value)
-    }
+  /** テキスト入力 */
+  const onChangeTextAreaEl = (event:React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextAreaEl(event.target.value)
+  }
+
+  /** 文字数の表示*/
+  const countWords = () => {
+    if (textAreaEl === "") setWordNum(0);
+    setWordNum(textAreaEl!.length)
+  }
+  /** 文字数の制限 */
+  //140文字超えたらボタンが押せなくなる。
+  const handleisDisabled = () => {
+    if(WordNum > 140) return setIsDisabled(true);
+    else{return setIsDisabled(false)}
+  }
+  /**これはなに？ */
+  useEffect(()=>{
+    handleisDisabled()
+  },[WordNum])
 
     return {textAreaEl, onChangeTextAreaEl, setTextAreaEl}
 }
