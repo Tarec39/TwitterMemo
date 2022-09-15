df8812abedce2e8b04217449d7acd129ae0258b9
Tarek Ito <itcameranian@gmail.com>
2022-09-15 12:04:36 +0900

    [add]偽MakeTweetを追加_#12 本物のMakeTweetを使うと渡さないといけない値が増えて面倒だから偽物を用意した。

---
+++ client/src/components/scrach/FakeMakeTweet.tsx
@@ -0,0 +1,8 @@
+export const FakeMakeTweet = () => {
+    return(
+        <>
+        <textarea style={{resize:'none', width:'300px', height:'200px'}}/>
+        <button>ツイート</button><br></br>
+        </>
+    )
+}
