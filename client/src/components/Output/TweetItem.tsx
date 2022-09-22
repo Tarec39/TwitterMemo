83522604d93c0ebaae9694a8c43eac51f765112c
Tarek <itcameranian@gmail.com>
2022-09-22 15:51:44 +0900

    [update]ちょっとだけ整理

--- client/src/components/Output/TweetItem.tsx
+++ client/src/components/Output/TweetItem.tsx
@@ -1,13 +1,12 @@
 import { Tweet } from "../../types/Tweet";
-// import {AddThread} from "../AddThread"
 type props = {
     tweet: Tweet
 }
+
 export const TweetItem = (props: props) => {
     return(
         <>
-        <p style={{whiteSpace: 'pre-wrap'}}>{props.tweet.text}</p>
-        {/* <AddThread /> */}
+        <p id="test" style={{whiteSpace: 'pre-wrap'}}>{props.tweet.text}</p>
         </>
     )
 }
