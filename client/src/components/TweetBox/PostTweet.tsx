7441e5b6ea91202c71458f86b4adfa6acb8bff08
Tarek Ito <itcameranian@gmail.com>
2022-10-04 13:25:08 +0900

    asd

--- client/src/components/TweetBox/PostTweet.tsx
+++ client/src/components/TweetBox/PostTweet.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import {Title} from './Title'
 
@@ -10,12 +11,16 @@ type props = {
 export const PostTweet = (props: props) => {
     return(
         <>
-        <h3>ツイート機能</h3>
-        <Title
-            value={props.inputEl}
-            onChange={props.onInputElChange}
-        /> 
+            <h3>ツイート機能</h3>
+
+            <Title
+                value={props.inputEl}
+                onChange={props.onInputElChange}
+            />  
         </>
     )
 }
 
+const TweetBox = styled.div`
+
+`
