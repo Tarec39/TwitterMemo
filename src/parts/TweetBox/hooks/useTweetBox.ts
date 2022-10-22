aaca5f295fc41a11ae8b181bb500f71645df30b2
Tarek <itcameranian@gmail.com>
2022-10-22 14:23:08 +0900

    refactor: シンプルにするため、簡単にした。

--- src/parts/TweetBox/hooks/useTweetBox.ts
+++ src/parts/TweetBox/hooks/useTweetBox.ts
@@ -1,10 +1,25 @@
 import {useState} from 'react'
 
 export const useTitle = () => {
-  const [value, setValue] = useState('')
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
+  const [inputEl, setInputEl] = useState<string>('')
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputEl(e.target.value)
   }
 
-  return {value, onChange}
+  return {inputEl, onChangeInput}
+}
+
+export const useText = () => {
+  const [textAreaEl, setTextAreaEl] = useState<string>('')
+
+  const onChangeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextAreaEl(e.target.value)
+  }
+
+  const calcRow = () => {
+    let rowNums = textAreaEl.split('\n').length
+    return rowNums
+  }
+
+  return{textAreaEl, onChangeTextArea, calcRow}
 }
--- src/parts/TweetBox/index.tsx
+++ src/parts/TweetBox/index.tsx
@@ -1,13 +1,26 @@
+//Parts
 import { Title } from "./components/Title";
+import { Text } from "./components/Text";
+//Hooks
 import { useTitle } from "./hooks/useTweetBox";
+import {useText} from "./hooks/useTweetBox"
 
 export const TweetBox = () => {
-    const {value, onChange} = useTitle()
+    const {inputEl, onChangeInput} = useTitle()
+    const {textAreaEl, onChangeTextArea, calcRow} = useText()
 
     return(
+        <>
         <Title 
-            value={value}
-            onChange={onChange}
+            value={inputEl}
+            onChange={onChangeInput}
+            />
+
+        <Text
+            value={textAreaEl}
+            onChange={onChangeTextArea}
+            rows={calcRow}
         />
+        </>
     )
 }
