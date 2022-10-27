a4162650782c3ed37d1bd19182fd70b6727a8fc6
Tarek <itcameranian@gmail.com>
2022-10-27 21:35:37 +0900

    なんかここに処理を書いて。

--- src/parts/TweetBox/components/Text.tsx
+++ src/parts/TweetBox/components/Text.tsx
@@ -3,17 +3,22 @@ import {useEffect, useState} from 'react'
 
 export const Text = () => {
     const [editorState, setEditorState] = useState(
-        () => EditorState.createEmpty(),
-      );
+      () => EditorState.createEmpty()
+    )
+
+    const handleOnClick = () => console.log('');
+
+
     return(
         <>
         <div>
           <h1>editor</h1>
-          <div>
+          <div style={{width: '100px'}}>
           <Editor
             editorState={editorState}
             onChange={setEditorState}
             />
+          <button onClick={handleOnClick}>確認</button>
           </div>
         </div>
     </>
