92fea5f417ad3431ef02eced080aaf339108504a
Tarek <itcameranian@gmail.com>
2022-11-11 10:49:25 +0900

    aaa

--- src/parts/Feed/index.tsx
+++ src/parts/Feed/index.tsx
@@ -8,15 +8,11 @@ import { TweetList } from "../TweetBox/components/share"
 
 import { Tweets } from "./components/Tweets"
 import { TweetData } from "../../types/Tweet"
-// import { deleteTweet } from "../../hooks/TweetApi"
 
 export const Feed = () => {
 
     const tweetList = useContext(TweetList)
-    const deleteTweet = useContext(TweetList)
-    // const {deleteTweet} = useTweet()
     const handleDelete = (id:string) => {
-        console.log('aa')
         tweetList?.deleteTweet(id)
     }
 
